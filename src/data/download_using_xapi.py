a86458de72377233ccbec114cb25c59b53b945af
Greg De Stigter <gdestigter@esri.com>
2013-10-21 14:17:35 -0700

    Fix to locale issues with XAPI download python tool
    Changed XAPI download python tool and toolbox to handle other locales.
    Fix for issue #30.

--- src/data/download_using_xapi.py
+++ src/data/download_using_xapi.py
@@ -1,18 +1,19 @@
 # Import system modules and arcpy
 #
 import arcpy, sys, urllib, urllib2
+import locale
 
 def xapidownload():
 	try:
 		# get the input XAPI url
 		#
 		in_xapi_url = arcpy.GetParameterAsText(0)
-		
-		# get the request extent 
+
+		# get the request extent
 		#
 		request_extent = arcpy.GetParameterAsText(1)
 		AddMsgAndPrint(request_extent)
-		
+
 		# if no extent was set and the default is passed in  -- meaning "#" then set an empty string
 		if (request_extent == '#'):
 			request_extent = ''
@@ -20,7 +21,7 @@ def xapidownload():
 			request_extent = ''
 		else:
 			coordinates = request_extent.split(' ')
-			request_extent = coordinates[0] + ',' +  coordinates[1] + ',' + coordinates[2] + ',' + coordinates[3]
+			request_extent = str(locale.atof(coordinates[0])) + ',' +  str(locale.atof(coordinates[1])) + ',' + str(locale.atof(coordinates[2])) + ',' + str(locale.atof(coordinates[3]))
 			request_extent = CheckForBrackets('bbox=' + request_extent)
 			request_extent = urllib.quote_plus(request_extent)
 
@@ -33,7 +34,7 @@ def xapidownload():
 		#
 		request_type = arcpy.GetParameterAsText(2)
 		#request_type = urllib.quote(request_type)
-		
+
 		# get the predicate string
 		#
 		request_predicate = arcpy.GetParameterAsText(3)
@@ -44,13 +45,13 @@ def xapidownload():
 		# get the location for the downloaded file
 		#
 		xapi_osm_file = arcpy.GetParameterAsText(4)
-		
+
 		# assemble the request url
 		# sample: http://jxapi.openstreetmap.org/xapi/api/0.6/*%5Bname=Sylt%5D
 		#
 		request_url = in_xapi_url + request_type + request_predicate + request_extent
 		AddMsgAndPrint(request_url)
-		
+
 		# issue the request against the XAPI endpoint
 		#
 		try:
@@ -62,40 +63,40 @@ def xapidownload():
 			elif hasattr(e, 'code'):
 				AddMsgAndPrint('The server was unable to fulfill the request.', 2)
 				AddMsgAndPrint(e.code, 2)
-				
+
 		else:
 			# write the content into a file on disk
 			#
 			with open(xapi_osm_file,'w') as xapifile:
 				xapifile.write(xapi_response.read())
-		
+
 	except Exception as err:
 		AddMsgAndPrint(err.message, 2)
-		
+
 
 def CheckForBrackets(inputString):
 	paddedString = inputString
-	
+
 	if (paddedString[0] != '['):
 		paddedString = '[' + paddedString
-		
+
 	if (paddedString[-1] != ']'):
 		paddedString = paddedString + ']'
-		
+
 	return paddedString
-	
+
 
 def AddMsgAndPrint(msg, severity=0):
     # Adds a Message (in case this is run as a tool)
     # and also prints the message to the screen (standard output)
-    # 
+    #
     print msg
 
-    # Split the message on \n first, so that if it's multiple lines, 
+    # Split the message on \n first, so that if it's multiple lines,
     #  a GPMessage will be added for each line
     try:
         for string in msg.split('\n'):
-            # Add appropriate geoprocessing message 
+            # Add appropriate geoprocessing message
             #
             if severity == 0:
                 arcpy.AddMessage(string)
